6c7efc31f5dd8ba5134fda04b112d04424becebe
MizuRyu <02rlmdev@gmail.com>
2024-06-14 19:37:09 +0900

    feat streaming chat ui

---
+++ app/chatapp.py
@@ -0,0 +1,190 @@
+import streamlit as st
+from openai import AzureOpenAI
+import dotenv
+import os
+from PIL import Image
+import base64
+from io import BytesIO
+
+dotenv.load_dotenv()
+
+
+# Function to query and stream the response from the LLM
+def stream_llm_response(client, model_params):
+    response_message = ""
+
+    for chunk in client.chat.completions.create(
+        model="gpt-4o",
+        messages=st.session_state.messages,
+        temperature=model_params["temperature"] if "temperature" in model_params else 0.3,
+        max_tokens=4096,
+        stream=True,
+    ):
+        choice = chunk.choices[0] if hasattr(chunk, 'choices') and chunk.choices else ""
+        if hasattr(choice, 'delta') and hasattr(choice.delta, 'content'):
+            content = chunk.choices[0].delta.content
+            response_message += content if content else ""
+            yield content
+
+    st.session_state.messages.append({
+        "role": "assistant", 
+        "content": [
+            {
+                "type": "text",
+                "text": response_message,
+            }
+        ]})
+
+
+# Function to convert file to base64
+def get_image_base64(image_raw):
+    buffered = BytesIO()
+    image_raw.save(buffered, format=image_raw.format)
+    img_byte = buffered.getvalue()
+
+    return base64.b64encode(img_byte).decode('utf-8')
+
+
+
+def main():
+
+    # --- Page Config ---
+    st.set_page_config(
+        page_title="The OmniChat",
+        page_icon="🤖",
+        layout="centered",
+        initial_sidebar_state="expanded",
+    )
+
+    # --- Header ---
+    st.html("""<h1 style="text-align: center; color: #6ca395;">🤖 <i>Chat</i> 💬</h1>""")
+
+    
+    # --- Main Content ---
+    # Checking if the user has introduced the OpenAI API Key, if not, a warning is displayed
+    if os.getenv("AZURE_OPENAI_API_KEY") is None:
+        st.write("#")
+        st.warning("⬅️ Please introduce your OpenAI API Key (make sure to have funds) to continue...")
+
+        with st.sidebar:
+            st.write("#")
+            st.write("#")
+
+    else:
+        client = AzureOpenAI(
+            api_version=os.getenv("API_VERSION"),
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        )
+
+
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Displaying the previous messages if there are any
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                for content in message["content"]:
+                    if content["type"] == "text":
+                        st.write(content["text"])
+                    elif content["type"] == "image_url":      
+                        st.image(content["image_url"]["url"])
+
+        # Side bar model options and inputs
+        with st.sidebar:
+
+            st.divider()
+
+            model = st.selectbox("Select a model:", [
+                "gpt-4o-2024-05-13", 
+                "gpt-4-turbo", 
+                "gpt-3.5-turbo-16k", 
+                "gpt-4", 
+                "gpt-4-32k",
+            ], index=0)
+            
+            with st.popover("⚙️ Model parameters"):
+                model_temp = st.slider("Temperature", min_value=0.0, max_value=2.0, value=0.3, step=0.1)
+
+            model_params = {
+                "temperature": model_temp,
+            }
+
+            def reset_conversation():
+                if "messages" in st.session_state and len(st.session_state.messages) > 0:
+                    st.session_state.pop("messages", None)
+
+            st.button(
+                "🗑️ Reset conversation", 
+                on_click=reset_conversation,
+            )
+
+            st.divider()
+
+            # Image Upload
+            if model in ["gpt-4o-2024-05-13", "gpt-4-turbo"]:
+                    
+                st.write("### **🖼️ Add an image:**")
+
+                def add_image_to_messages():
+                    if st.session_state.uploaded_img or ("camera_img" in st.session_state and st.session_state.camera_img):
+                        img_type = st.session_state.uploaded_img.type if st.session_state.uploaded_img else "image/jpeg"
+                        raw_img = Image.open(st.session_state.uploaded_img or st.session_state.camera_img)
+                        img = get_image_base64(raw_img)
+                        st.session_state.messages.append(
+                            {
+                                "role": "user", 
+                                "content": [{
+                                    "type": "image_url",
+                                    "image_url": {"url": f"data:{img_type};base64,{img}"}
+                                }]
+                            }
+                        )
+
+                cols_img = st.columns(2)
+
+                with cols_img[0]:
+                    with st.popover("📁 Upload"):
+                        st.file_uploader(
+                            "Upload an image", 
+                            type=["png", "jpg", "jpeg"], 
+                            accept_multiple_files=False,
+                            key="uploaded_img",
+                            on_change=add_image_to_messages,
+                        )
+
+                with cols_img[1]:                    
+                    with st.popover("📸 Camera"):
+                        activate_camera = st.checkbox("Activate camera")
+                        if activate_camera:
+                            st.camera_input(
+                                "Take a picture", 
+                                key="camera_img",
+                                on_change=add_image_to_messages,
+                            )
+
+
+
+        # Chat input
+        if prompt := st.chat_input("Hi! Ask me anything..."):
+            st.session_state.messages.append(
+                {
+                    "role": "user", 
+                    "content": [{
+                        "type": "text",
+                        "text": prompt,
+                    }]
+                }
+            )
+            
+            # Displaying the new messages
+            with st.chat_message("user"):
+                st.markdown(prompt)
+
+            with st.chat_message("assistant"):
+                st.write_stream(
+                    stream_llm_response(client, model_params)
+                )
+
+if __name__=="__main__":
+    main()
