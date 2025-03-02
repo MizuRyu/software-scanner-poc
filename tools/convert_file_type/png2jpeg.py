98ec0e3499e8c7e3142cd39a07b6a479d025d039
MizuRyu <02rlmdev@gmail.com>
2025-03-02 22:47:41 +0900

    fix

--- tools/convert_file_type/png2jpeg.py
+++ tools/convert_file_type/png2jpeg.py
@@ -2,15 +2,14 @@ from PIL import Image
 import os
 import datetime
 
-input_path = os.path.join(os.getcwd(), 'pngs')
-output_dir = os.path.join(os.getcwd(), 'imgs')
+sample_dir = os.path.join(os.getcwd(), 'sample')
 
 # 出力ディレクトリが存在しない場合は作成
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+if not os.path.exists(sample_dir):
+    os.makedirs(sample_dir)
 
 # 指定ディレクトリのファイルを取得
-files = os.listdir(input_path)
+files = os.listdir(sample_dir)
 print(f"files: {files}")
 
 # 取得したファイルからPNGファイルのみ取得
@@ -21,16 +20,18 @@ dt_now = datetime.datetime.now().strftime('%y%m%d_%H%M%S')
 
 # pngをjpgに変換する
 for p in pngs:
-    input_img_path = os.path.join(input_path, p)
-    output_img_name = os.path.splitext(p)[0] + '.jpg'
-    output_img_path = os.path.join(output_dir, output_img_name)
+    png_path = os.path.join(sample_dir, p)
+    img_file_name = os.path.splitext(p)[0] + '.jpg'
+    img_path = os.path.join(sample_dir, img_file_name)
     
-    input_img = Image.open(input_img_path)
+    input_img = Image.open(png_path)
     output_img = input_img.convert('RGB')
-    output_img.save(output_img_path, quality=40)
+    output_img.save(img_path, quality=40)
 
-    file_size = os.path.getsize(output_img_path)
-    print(f"Converted {p} to {output_img_name}, Size: {file_size / 1024:.2f} KB")
+    file_size = os.path.getsize(img_path)
+    print(f"Converted {p} to {img_file_name}, Size: {file_size / 1024:.2f} KB")
 
+    os.remove(png_path)
+    print(f"Removed {p}")
 
 print("finished")
